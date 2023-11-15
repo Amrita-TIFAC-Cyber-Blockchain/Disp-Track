1dac3f6bb355b58efb935d84088e62c00b40f03c
Ramaguru Radhakrishnan <7790256+ramagururadhakrishnan@users.noreply.github.com>
2023-11-15 19:00:56 +0530

    Updated path

--- UI/eth/src/App.jsx
+++ UI/eth/src/App.jsx
@@ -19,10 +19,10 @@ function App() {
           <BrowserRouter>
             <Navbar />
             <Routes>
-              <Route path="/Disp-Track/UI/eth/" element={<MainContent />}></Route>
-              <Route path="/Disp-Track/UI/eth/upload" element={<UploadForm />}></Route>
-              <Route path="/Disp-Track/UI/eth/retrieve" element={<RetrieveDetails />}></Route>
-              <Route path="*" element={<Navigate to="/Disp-Track/UI/eth/" />}></Route>
+              <Route path="/DisP-Track/UI/eth/" element={<MainContent />}></Route>
+              <Route path="/DisP-Track/UI/eth/upload" element={<UploadForm />}></Route>
+              <Route path="/DisP-Track/UI/eth/retrieve" element={<RetrieveDetails />}></Route>
+              <Route path="*" element={<Navigate to="/DisP-Track/UI/eth/" />}></Route>
             </Routes>
           </BrowserRouter>
         </StateProvider>
