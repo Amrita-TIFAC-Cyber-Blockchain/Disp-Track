4195037fa564ec02e7a179804e298a26dff1f83b
Ramaguru Radhakrishnan <7790256+ramagururadhakrishnan@users.noreply.github.com>
2023-11-15 18:40:59 +0530

    Update path

--- UI/eth/vite.config.js
+++ UI/eth/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/DispTrack/UI/eth/",
+  base: "/Disp-Track/UI/eth/",
   plugins: [react()],
 })
