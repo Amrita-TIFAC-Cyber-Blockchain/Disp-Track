863f88586fac49b76299825dc5081543dc95e58c
Ramaguru Radhakrishnan <7790256+ramagururadhakrishnan@users.noreply.github.com>
2023-11-15 20:03:54 +0530

    Update vite.config.js

--- UI/eth/vite.config.js
+++ UI/eth/vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/DisP-Track/UI/eth/",
   plugins: [react()],
+  build: {
+    outDir: 'dist', // The output directory for the production build
+    assetsDir: '', // Assets directory relative to outDir
+    minify: 'terser', // Use 'terser' for minification
+  },
 })
